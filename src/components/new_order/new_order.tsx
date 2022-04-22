7cd7ffed3826b9e79f3acd41953dc42919201a04
daniel daum <daniel_daum@outlook.com>
2022-04-21 17:59:10 -0700

    idk

--- src/components/new_order/new_order.tsx
+++ src/components/new_order/new_order.tsx
@@ -5,27 +5,23 @@ function New_Order() {
     return (
         <div className="new-order">
 
-            <h1 className="new-order-title">Add New Order</h1>
-
+            <div className="new-order-title">Add New Order</div>
 
             <form className="new-order-wrapper" action="#">
 
                 <div>
-                    <label htmlFor="new-order-item-name">Product Name</label>
+                    <label htmlFor="new-order-item-name">Item Name</label>
                     <input type="text" placeholder="Product Name" id="item-name" />
                 </div>
 
                 <div>
                     <label htmlFor="new-order-item-date">Order Date</label>
-                    <input type="date" placeholder="Item Name" id="item-name"  />
+                    <input type="date" placeholder="Item Name" id="item-name" />
                 </div>
 
                 <div>
-                    
+
                 </div>
-
-
-
 
                 <input type="text" placeholder="Item Name" id="item-name" />
                 <input type="text" placeholder="Item Name" id="item-name" />
